e78ef6cc543c6a8838164efc910fac98dd49de60
Adam S <adam.sarli@gmail.com>
2019-05-03 19:48:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,6 @@ class PigLatinizer
   
   def piglatinize(word)
     fletter = word[0].downcase 
-    binding.pry 
     if fletter != "a" && fletter != "e" && fletter != "i" && fletter != "o" && fletter != "u"
       word[1..] + word[0] + "ay"
     else
