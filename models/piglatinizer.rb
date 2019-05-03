e9513fe4c5a09494a17cffd03dd796091ebc11c9
Adam S <adam.sarli@gmail.com>
2019-05-03 20:01:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,14 +6,15 @@ class PigLatinizer
   end 
   
   def piglatinize(word)
+    pig_word(word)
+  end 
+  
+  def pig_word(word)
     fletter = word.split(/[aeiouAEIOU]/)[0]
     if fletter == nil || fletter == ""
       word + "way"
     else 
       word.split(fletter)[1] + fletter + "ay"
     end 
-  end 
-  
-  def pig_word(word)
   end 
 end 
