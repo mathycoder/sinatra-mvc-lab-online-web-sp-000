490263ccf4ce9287c2cacad8b8c48dcf49634916
Adam S <adam.sarli@gmail.com>
2019-05-03 19:41:05 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,6 +10,7 @@ class PigLatinizer
     if word[0] != 'a' || word[0] != 'e' word[0] != 'i' word[0] != 'o' word[0] != 'u'
       word[1..] + word[0] + "ay"
     else
+      word
     end 
   end 
 end 
