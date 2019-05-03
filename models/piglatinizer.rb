040cdcf9bd2981fd7fe560577e80e71deb1af1f1
Adam S <adam.sarli@gmail.com>
2019-05-03 19:41:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,9 @@ class PigLatinizer
   
   def piglatinize(word)
     word = word.downcase 
-    if word[0] != 'a'
-    word[1..] + word[0] + "ay"
+    if word[0] != 'a' || word[0] != 'e' word[0] != 'i' word[0] != 'o' word[0] != 'u'
+      word[1..] + word[0] + "ay"
+    else
+    end 
   end 
 end 
